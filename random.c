9e51f389a0a8ca93faf9e75cff119ff2461cfb5b
Мельникова <145825421+softsoftsofta@users.noreply.github.com>
2025-06-09 00:27:51 +0500

    Delete random.c

--- random.c
+++
@@ -1,9 +0,0 @@
-#include "random.h"
-
-#include <stdlib.h>
-
-void randomFill(arr_t array[], const uint32_t size) {
-  #pragma omp parallel for
-  for(uint32_t i=0; i<size; ++i)
-    array[i] = rand()%MAX_RAND;
-}
