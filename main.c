89ef1486db9c2fded80403ee4667137a270a0b64
Мельникова <145825421+softsoftsofta@users.noreply.github.com>
2025-06-09 06:53:04 +0500

    Update main.c

--- main.c
+++ main.c
@@ -6,78 +6,135 @@
 #define N 2500000
 #define ITERATIONS 1000
 
-void sequential_operations(double* a, double* b, double* add, double* sub, double* mul, double* div) {
-    for (int i = 0; i < N; i++) {
-        add[i] = a[i] + b[i];
-        sub[i] = a[i] - b[i];
-        mul[i] = a[i] * b[i];
-        if (b[i] != 0.0) {
-            div[i] = a[i] / b[i];
-        }
-        else {
-            div[i] = 0.0;
-        }
-    }
-}
-
-void parallel_operations(double* a, double* b, double* add, double* sub, double* mul, double* div, int num_threads) {
-#pragma omp parallel for num_threads(num_threads)
-    for (int i = 0; i < N; i++) {
-        add[i] = a[i] + b[i];
-        sub[i] = a[i] - b[i];
-        mul[i] = a[i] * b[i];
-        if (b[i] != 0.0) {
-            div[i] = a[i] / b[i];
-        }
-        else {
-            div[i] = 0.0;
-        }
-    }
-}
-
-void initialize_arrays(double* a, double* b) {
+// Инициализация массивов
+void initialize_arrays(double *a, double *b) {
     for (int i = 0; i < N; i++) {
         a[i] = (double)rand() / RAND_MAX * 100.0;
         b[i] = (double)rand() / RAND_MAX * 100.0 + 1.0; // избегаем деления на ноль
     }
 }
 
-double measure_time(void (*func)(double*, double*, double*, double*, double*, double*, int),
-    double* a, double* b, double* add, double* sub, double* mul, double* div,
-    int num_threads) {
+// Последовательные операции
+void add_sequential(double *a, double *b, double *res) {
+    for (int i = 0; i < N; i++) {
+        res[i] = a[i] + b[i];
+    }
+}
+
+void sub_sequential(double *a, double *b, double *res) {
+    for (int i = 0; i < N; i++) {
+        res[i] = a[i] - b[i];
+    }
+}
+
+void mul_sequential(double *a, double *b, double *res) {
+    for (int i = 0; i < N; i++) {
+        res[i] = a[i] * b[i];
+    }
+}
+
+void div_sequential(double *a, double *b, double *res) {
+    for (int i = 0; i < N; i++) {
+        if (b[i] != 0.0) {
+            res[i] = a[i] / b[i];
+        } else {
+            res[i] = 0.0;
+        }
+    }
+}
+
+// Параллельные операции
+void add_parallel(double *a, double *b, double *res, int num_threads) {
+    int i;
+#pragma omp parallel for num_threads(num_threads)
+    for (i = 0; i < N; i++) {
+        res[i] = a[i] + b[i];
+    }
+}
+
+void sub_parallel(double *a, double *b, double *res, int num_threads) {
+    int i;
+#pragma omp parallel for num_threads(num_threads)
+    for (i = 0; i < N; i++) {
+        res[i] = a[i] - b[i];
+    }
+}
+
+void mul_parallel(double *a, double *b, double *res, int num_threads) {
+    int i;
+#pragma omp parallel for num_threads(num_threads)
+    for (i = 0; i < N; i++) {
+        res[i] = a[i] * b[i];
+    }
+}
+
+void div_parallel(double *a, double *b, double *res, int num_threads) {
+    int i;
+#pragma omp parallel for num_threads(num_threads)
+    for (i = 0; i < N; i++) {
+        if (b[i] != 0.0) {
+            res[i] = a[i] / b[i];
+        } else {
+            res[i] = 0.0;
+        }
+    }
+}
+
+// Замер времени для одной операции
+double measure_time(void (*op)(double*, double*, double*, int), 
+                    double *a, double *b, double *res, 
+                    int num_threads) {
     double start_time = omp_get_wtime();
     for (int i = 0; i < ITERATIONS; i++) {
-        func(a, b, add, sub, mul, div, num_threads);
+        op(a, b, res, num_threads);
     }
     double end_time = omp_get_wtime();
     return (end_time - start_time) / ITERATIONS;
 }
 
 int main() {
-    double* a = (double*)malloc(N * sizeof(double));
-    double* b = (double*)malloc(N * sizeof(double));
-    double* add = (double*)malloc(N * sizeof(double));
-    double* sub = (double*)malloc(N * sizeof(double));
-    double* mul = (double*)malloc(N * sizeof(double));
-    double* div = (double*)malloc(N * sizeof(double));
+    double *a = (double*)malloc(N * sizeof(double));
+    double *b = (double*)malloc(N * sizeof(double));
+    double *add = (double*)malloc(N * sizeof(double));
+    double *sub = (double*)malloc(N * sizeof(double));
+    double *mul = (double*)malloc(N * sizeof(double));
+    double *div = (double*)malloc(N * sizeof(double));
 
     srand(time(NULL));
     initialize_arrays(a, b);
 
     // Последовательное выполнение
-    double seq_time = measure_time((void (*)(double*, double*, double*, double*, double*, double*, int))sequential_operations,
-        a, b, add, sub, mul, div, 1);
-    printf("Sequential time: %.6f seconds per operation\n", seq_time);
+    printf("Sequential execution:\n");
+    double add_seq_time = measure_time(add_sequential, a, b, add, 1);
+    double sub_seq_time = measure_time(sub_sequential, a, b, sub, 1);
+    double mul_seq_time = measure_time(mul_sequential, a, b, mul, 1);
+    double div_seq_time = measure_time(div_sequential, a, b, div, 1);
+
+    printf("  Addition: %.6f sec/op\n", add_seq_time);
+    printf("  Subtraction: %.6f sec/op\n", sub_seq_time);
+    printf("  Multiplication: %.6f sec/op\n", mul_seq_time);
+    printf("  Division: %.6f sec/op\n\n", div_seq_time);
 
     // Параллельное выполнение с разным количеством потоков
-    int thread_counts[] = { 2, 4, 8, 16 };
+    int thread_counts[] = {2, 4, 8, 16};
     int num_tests = sizeof(thread_counts) / sizeof(thread_counts[0]);
 
     for (int i = 0; i < num_tests; i++) {
         int threads = thread_counts[i];
-        double par_time = measure_time(parallel_operations, a, b, add, sub, mul, div, threads);
-        printf("Parallel time (%d threads): %.6f seconds per operation\n",
-            threads, par_time, seq_time / par_time);
+        printf("Parallel execution (%d threads):\n", threads);
+
+        double add_par_time = measure_time(add_parallel, a, b, add, threads);
+        double sub_par_time = measure_time(sub_parallel, a, b, sub, threads);
+        double mul_par_time = measure_time(mul_parallel, a, b, mul, threads);
+        double div_par_time = measure_time(div_parallel, a, b, div, threads);
+printf("  Addition: %.6f sec/op (Speedup: %.2f)\n", 
+               add_par_time, add_seq_time / add_par_time);
+        printf("  Subtraction: %.6f sec/op (Speedup: %.2f)\n", 
+               sub_par_time, sub_seq_time / sub_par_time);
+        printf("  Multiplication: %.6f sec/op (Speedup: %.2f)\n", 
+               mul_par_time, mul_seq_time / mul_par_time);
+        printf("  Division: %.6f sec/op (Speedup: %.2f)\n\n", 
+               div_par_time, div_seq_time / div_par_time);
     }
 
     free(a);
